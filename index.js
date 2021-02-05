5f10418511563dc1848483f20770de6f8d9b43ef
Mohammad Agbareya <muhammed@irestweb.com>
2021-02-06 00:57:56 +0200

    wip

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const arrayfy = payload => Array.isArray(payload) ? payload : [ payload ]
 
 export default {
-    install(Vue, { supabaseUrl, supabaseKey, store }) {
+    install(Vue, { supabaseUrl, supabaseKey, store, tables }) {
 
         const supabase = createClient(supabaseUrl, supabaseKey)
 
@@ -22,7 +22,7 @@ export default {
         Vue.prototype.$supa = table => {
             table = supabase.from(table)
             return {
-                get: async ({ select, orderby, dir }) => await table.select(select || '*').order(orderby || 'id', { ascending: dir != 'desc' }),
+                get: async ({ select, orderBy, dir } = {}) => await table.select(select || '*').order(orderBy || 'id', { ascending: dir != 'desc' }),
                 insert: async payload => await table.insert(arrayfy(payload)),
                 delete: async ids => await table.delete().in('id', arrayfy(ids)),
                 update: async (ids, payload) => await table.update(payload).in('id', arrayfy(ids)),
@@ -31,48 +31,116 @@ export default {
         
         Vue.prototype.$supaAuth = supabase.auth
 
-        Vue.prototype.$supaWatch = async (table, list /*, callbacks*/) => {
+        // Vue.prototype.$supaWatch = async (table, list /*, callbacks*/) => {
 
-            supabase.from(table).on('*', ({ eventType, new: row, old: deleted }) => {
-                if(eventType == 'INSERT')
-                    list.push(row)
-                else if(['DELETE', 'UPDATE'].includes(eventType)) {
-                    let index = list.findIndex(q => q.id == (deleted?.id || row.id))
-                    Vue.set(list, index, row)
-                    if(deleted?.id)
-                        Vue.delete(list, index)
-                }
-            })
-            .subscribe()
+        //     // supabase.from(table).on('*', ({ eventType, new: row, old: deleted }) => {
+        //     //     if(eventType == 'INSERT')
+        //     //         list.push(row)
+        //     //     else if(['DELETE', 'UPDATE'].includes(eventType)) {
+        //     //         let index = list.findIndex(q => q.id == (deleted?.id || row.id))
+        //     //         Vue.set(list, index, row)
+        //     //         if(deleted?.id)
+        //     //             Vue.delete(list, index)
+        //     //     }
+        //     // })
+        //     // .subscribe()
 
-            let { data, error } = await supabase.from(table).select('*').order('id')
+        //     let { data, error } = await supabase.from(table).select('*').order('id')
 
-            if(error)
-                throw error
+        //     if(error)
+        //         throw error
 
-            for (let i = 0; i < data.length; i++) {
-                Vue.set(list, i, data[i])
-            }
+        //     for (let i = 0; i < data.length; i++) {
+        //         Vue.set(list, i, data[i])
+        //     }
 
-        }
+        // }
 
         if(store) {
             store.registerModule('vueSupa', {
                 state: {
                     $supaUser: null,
+                    $tables: {},
                 }, 
                 mutations: {
-                    supauserStateChanged: (state, payload) => state.$supaUser = payload
+                    supaUserStateChanged: (state, payload) => state.$supaUser = payload,
+                    supaTableGet: (state, { name, data }) => Vue.set(state.$tables, name, data),
+                    supaTableInsert: (state, { name, row }) => state.$tables[name].push(row),
+                    supaTableDelete: (state, { name, id }) => {
+                        let index = state.$tables[name].findIndex(q => q.id == id)
+                        Vue.delete(state.$tables[name], index)
+                    },
+                    supaTableUpdate: (state, { name, id, row }) => {
+                        let index = state.$tables[name].findIndex(q => q.id == id)
+                        Vue.set(state.$tables[name], index, row)
+                    },
                 }, 
                 // actions,
                 getters: {
                     $supaUser: ({ $supaUser }) => $supaUser,
+                    $supaTable: ({ $tables }) => (table => $tables[table]),
+                    $supaTables: ({ $tables }) => $tables,
                 }
             });
     
             (store => {
                 supabase.auth.onAuthStateChange((event, session) => {
-                    store.commit('supauserStateChanged', event == 'SIGNED_IN' ? session.user : null)
+                    store.commit('supaUserStateChanged', event == 'SIGNED_IN' ? session.user : null)
+                    tables.forEach(table => {
+                        supabase.from(table.name || table)
+                            .select(table.select || '*')
+                            .order('id')
+                            .then(({ data }) => {
+                                console.log('>>>', data)
+                                store.commit('supaTableGet', {
+                                    name: table.name || table,
+                                    data,
+                                })
+                            })
+    
+                        supabase.from(table.name || table)
+                            .on('INSERT', ({ new: row }) => {
+                                store.commit('supaTableInsert', {
+                                    name: table.name || table,
+                                    row,
+                                })
+                            })
+                            .on('DELETE', ({ old: deleted }) => {
+                                store.commit('supaTableDelete', {
+                                    name: table.name || table,
+                                    id: deleted.id,
+                                })
+                            })
+                            .on('UPDATE', ({ new: row }) => {
+                                store.commit('supaTableUpdate', {
+                                    name: table.name || table,
+                                    id: row.id,
+                                    row,
+                                })
+                            })
+                            // .on('*', ({ eventType, new: row, old: deleted }) => {
+                            //     if(eventType == 'INSERT') {
+                            //         store.commit('supaTableInsert', {
+                            //             name: table.name || table,
+                            //             row,
+                            //         })
+                            //     }
+                            //     else if(eventType == 'DELETE') {
+                            //         store.commit('supaTableDelete', {
+                            //             name: table.name || table,
+                            //             id: deleted.id,
+                            //         })
+                            //     }
+                            //     else if(eventType == 'UPDATE') {
+                            //         store.commit('supaTableUpdate', {
+                            //             name: table.name || table,
+                            //             id: row.id,
+                            //             row,
+                            //         })
+                            //     }
+                            // })
+                            .subscribe()
+                    })
                 })
             })(store)
         
@@ -80,7 +148,13 @@ export default {
                 computed: {
                     $supaUser() {
                         return this.$store.getters.$supaUser
-                    }
+                    },
+                    $supaTable() {
+                        return this.$store.getters.$supaTable
+                    },
+                    $supaTables() {
+                        return this.$store.getters.$supaTables
+                    },
                 }
             })
         }
